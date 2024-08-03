c95aa0438cb7822068f82be81f4d3a174bc2fe1e
Sharky83 <alex_brocky@hotmail.com>
2024-08-03 10:35:43 +0100

    updated css to relative path

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('./js/shopItems.json') 
+    fetch('./shopItems.json') 
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -45,3 +45,7 @@ function createItemElement(item) {
 
     return itemElement;
 }
+
+
+
+
